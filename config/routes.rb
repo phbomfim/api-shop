12a53a1d07db3e6dcaaeccca2c8ec0434a4de92d
phbomfim <paulobommfim@gmail.com>
2021-04-27 15:36:14 -0300

    Define home route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
       root to: "orders#index"
     end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: 'home#index'
+
 end
