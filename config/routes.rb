be43b3b48614d3e78e1b9b88d16b1281330c6438
phbomfim <paulobommfim@gmail.com>
2021-04-27 12:12:38 -0300

    Add administrate gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :orders
+      resources :products
+      resources :promotions
+
+      root to: "orders#index"
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
