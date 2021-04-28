5ecd4e8fc92f7d018a013309364a6b931b4df107
phbomfim <paulobommfim@gmail.com>
2021-04-28 11:53:45 -0300

    Switch product to order in home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
     def index
         @products = Product.order(published_at: :desc)
+        @orders = Order.order(published_at: :desc)
     end
 
     def terms
@@ -8,4 +9,11 @@ class HomeController < ApplicationController
 
     def privacy
     end
+
+    private
+
+        def description
+            @order_product_id = Order.find_by(params[:id]).product_id
+            @product = Product.find_by(params[:id])
+        end
 end
