5509cb76859a27501f8972306501e63dfd28b7ca
phbomfim <paulobommfim@gmail.com>
2021-04-28 00:52:57 -0300

    Create a stock method check

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,17 +2,17 @@ module Admin
     class Api::V1::ProductsController < ApiController
         before_action :set_product
         
-        def show
+        def index
             render json: @product
         end
 
         private
 
             def set_product
-                @product = Product.all
-                @product.each do |p|
-                    if p.stock > 0
-                        @product = p
+                @product = []               #Create array to include Product variables
+                Product.all.each do |p|
+                    if p.stock > 0          #Check if stock is not empty
+                        @product.push(p)    #Add to array
                     end
                 end
             end
