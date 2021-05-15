6b5984f1f213a41400fc7694aafbd1170c072279
phbomfim <paulobommfim@gmail.com>
2021-05-15 16:37:32 -0300

    Remove admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  #Admin dashboard route
-  namespace :admin do
-    resources :orders
-    resources :products
-    resources :promotions
-
-    root to: "orders#index"
-  end
-  
   #API route
   namespace :api do
     namespace :v1 do
